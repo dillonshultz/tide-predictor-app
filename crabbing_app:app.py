ac211c45d151d18528bff1524e525ff116e779d6
dillonshultz <dillonshultz1@gmail.com>
2026-02-10 21:20:36 -0800

    Update crabbing_app:app.py

--- crabbing_app:app.py
+++ crabbing_app:app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.markdown("""
     <style>
     .stApp {
-        background-color: #c21807;
+        background-color: #1c2951;
     }
     </style>
 """, unsafe_allow_html=True)
