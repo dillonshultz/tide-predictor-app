faebe623f46f9c60c0a4e942f30a2a9c944400c5
Dillon <dillonshultz1@gmail.com>
2026-04-24 10:32:08 -0700

    Change background color of the app

--- crabbing_app:app.py
+++ crabbing_app:app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.markdown("""
     <style>
     .stApp {
-        background-color: #0e4c92;
+        background-color: #87CEFA;
     }
     </style>
 """, unsafe_allow_html=True)
